cc70998c1a6dfd8d7a5126d751cd6bea0a4d141e
Nyi Bo <alexenderjunior6@gmail.com>
2023-09-21 17:18:59 +0630

    built in interface (Interface)

---
+++ part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Set;
+import java.util.HashSet;
+public class Main {
+
+    public static void main(String[] args) {
+        // You can test your method here
+        Set<String> names = new HashSet<>();
+        names.add("first");
+        names.add("first");
+        names.add("second");
+        names.add("second");
+        names.add("second");
+
+    }
+
+    // implement the method returnSize here, which returns
+    // the number of elements in the set that it receives as a parameter.
+    
+    public static int returnSize(Set<String> names){
+        return names.size();
+    }
+}
