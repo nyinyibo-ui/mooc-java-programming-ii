e0f4f385fbc2790311403d84b84c612ddf58a341
Nyi Bo <alexenderjunior6@gmail.com>
2023-10-06 20:05:02 +0630

    updated

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -12,9 +12,7 @@ public class Person {
             throw new IllegalArgumentException("age out of range");
         }
 
-        if (age < 0 || age > 120) {
-            throw new IllegalArgumentException("Age must be between 0 and 120");
-        }
+
         this.name = name;
         this.age = age;
     }
