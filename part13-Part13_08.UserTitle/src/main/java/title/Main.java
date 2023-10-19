24b4dfa7ed3c0d1267f90d4ccf8e08c11643ec26
Nyi Bo <alexenderjunior6@gmail.com>
2023-10-19 17:55:49 +0630

    user title (launch parameters)

---
+++ part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -0,0 +1,19 @@
+package title;
+
+import javafx.application.Application;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+
+        Application.launch(UserTitle.class,
+                "--course=" + title);
+    }
+
+}
+
+
